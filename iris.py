61924218b0ec82907094eab3e4aa420d3d2a828b
NamelessSuperCoder <Marius.juston@hotmail.fr>
2017-08-01 10:29:46 -0400

    used kfolds validation

--- iris.py
+++ iris.py
@@ -1,12 +1,14 @@
 from mpl_toolkits.mplot3d import Axes3D
 from sklearn.datasets import load_iris
+from sklearn.model_selection import KFold
+from sklearn.neighbors import KNeighborsClassifier
+import numpy as np
+import matplotlib.pyplot as plt
 
 data = load_iris()
 
-features = data['data'][:, [0, 2, 3]]
-labels = data['target']
-
-import matplotlib.pyplot as plt
+features = np.array( data['data'][:, [0, 2, 3]])
+labels = np.array(data['target'])
 
 fig = plt.figure()
 ax = Axes3D(fig)
@@ -20,7 +22,18 @@ features = scaller.fit_transform(features)
 colors = ['r', 'g', 'b']
 
 ax.scatter(features[:,0],features[:,1], features[:,2], c=labels)
-
-ax.cla()
-
 plt.show()
+
+clf = KNeighborsClassifier(n_neighbors=3)
+
+kf = KFold(n_splits=10, random_state=42, shuffle=True)
+
+for  train_index, test_index in kf.split(X):
+  train_x, train_y, test_x, test_y = features[train_index], labels[train_index] , features[test_index], lables[test_index]
+  
+  
+  
+  
+  
+  
+  
