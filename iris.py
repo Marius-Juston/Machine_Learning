5af331e6b30d25476e3c16873887eb54da7827e0
NamelessSuperCoder <Marius.juston@hotmail.fr>
2017-08-01 10:47:46 -0400

    added fitting, need to make accuracy printing method

--- iris.py
+++ iris.py
@@ -5,6 +5,11 @@ from sklearn.neighbors import KNeighborsClassifier
 import numpy as np
 import matplotlib.pyplot as plt
 
+from sklearn.metrics import recall_score, precision_score, f1_score, accuracy_score, confusion_matrix
+
+def get_accuracy(i, true_y, pred_y):
+  pass
+
 data = load_iris()
 
 features = np.array( data['data'][:, [0, 2, 3]])
@@ -28,10 +33,14 @@ clf = KNeighborsClassifier(n_neighbors=3)
 
 kf = KFold(n_splits=10, random_state=42, shuffle=True)
 
-for  train_index, test_index in kf.split(X):
+for i,  train_index, test_index in enumarate( kf.split(X)):
   train_x, train_y, test_x, test_y = features[train_index], labels[train_index] , features[test_index], lables[test_index]
   
+  clf.fit(train_x, train_y)
   
+  pred = clf.predict(test_x)
+  
+  get_accuracy(i, test_y, pred)
   
   
   
